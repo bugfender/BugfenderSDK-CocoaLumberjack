5fcaf82200827e636db5886a52f86e8a59fe21cc
David Lam <davidlys@gmail.com>
2017-01-26 21:16:49 +0800

    Swift port

---
+++ DDBugfenderLogger.swift
@@ -0,0 +1,54 @@
+//
+//  DDBugfenderLogger.swift
+//
+//  Created by David Lam on 26/1/17.
+//  Copyright © 2017. All rights reserved.
+//
+
+import Foundation
+import Bugfender
+import CocoaLumberjack
+
+class DDBugfenderLogger: DDAbstractLogger {
+    fileprivate static let _instance = DDBugfenderLogger()
+    
+    public static var shared: DDBugfenderLogger {
+        return _instance
+    }
+    
+    fileprivate override init() {
+        
+    }
+    
+    override func log(message logMessage: DDLogMessage!) {
+        guard let logMessage = logMessage else {
+            return
+        }
+
+        let message = logFormatter?.format(message: logMessage) ?? logMessage.message
+        
+        var logLevel: BFLogLevel!
+        switch logMessage.flag {
+        case DDLogFlag.error:
+            logLevel = BFLogLevel.error
+        case DDLogFlag.warning:
+            logLevel = BFLogLevel.warning
+        default:
+            logLevel = BFLogLevel.default
+        }
+        
+        let tagName = String(logMessage.context)
+        
+        Bugfender.log(withLineNumber: Int(logMessage.line),
+                      method: logMessage.function,
+                      file: logMessage.fileName,
+                      level: logLevel,
+                      tag: tagName,
+                      message: message)
+    }
+    
+    override var loggerName: String! {
+        return "com.bugfender.cocoalumberjack"
+    }
+    
+}
